46ca42887784565ca849a866e5cabdd640c751c7
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-11-25 11:22:43 +0000

    Update ospfdetector.py

--- ospfdetector.py
+++ ospfdetector.py
@@ -4,9 +4,13 @@ AUTHOR: IPvZero
 DATE: 24rd Nov 2020
 
 Purpose: Automated Check to check OSPF links are share the same network/mask, area and timers.
+Be aware that as CDP is used you will see the mismatch from both traffic directions 
+ie: it will show a failure from R1 to R2, as well as R2 back to R1.
+This is the same mismatch shown from both perspectives of each device.
 
 Scope: This script uses CDP and was designed to operate between directly connected, OSPF-enabled devices.
-Switched shared segments will break the CDP logic.
+Switched shared segments will break the CDP logic. 
+
 
 Notes: This script uses Nornir2, not Nornir3. Install with:
 pip3 install nornir"<3"
